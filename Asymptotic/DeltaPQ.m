757f5e38e41daa640170d356830d4c81d489c784
Wang Jiesen <jiesenwang@gmail.com>
2024-09-22 16:43:59 +0200

    Add files via upload

---
+++ Asymptotic/DeltaPQ.m
@@ -0,0 +1,6 @@
+C01V0V1
+
+gamma0 = 2+(p-q-(p+q)*p)/(n*q);
+gamma1 = -(p+q)/(n*q);
+SigmaSquare = gamma0^2*v0+2*gamma0*gamma1*c01+gamma1^2*v1
+TauSquare = SigmaSquare*q^2/p^2
---
+++ Asymptotic/checkVariancePQ.m
@@ -0,0 +1,6 @@
+ load('RecordPQ.mat')
+
+ C01V0V1
+
+ [var(Record(:,1))*K SigmaSquare]
+ [var(Record(:,2))*K TauSquare]
